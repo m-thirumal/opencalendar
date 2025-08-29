ad487bb86e8802b77898c0a1a3529cf9bfa36e69
Thirumal <m.thirumal@hotmail.com>
2025-08-29 21:45:22 +0530

    Git hub action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  output: "export", // enable static export
+  images: {
+    unoptimized: true, // required for GitHub Pages
+  },
+  basePath: isProd ? "/opencalendar" : "",
+};
 
 export default nextConfig;
